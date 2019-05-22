3f4d3119dea4a2af87fa904d900c5d67af470a7d
Martin Wilderoth <martin.wilderoth@linserv.se>
2019-05-22 16:32:29 +0200

    Updated conf.d in deploy script

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -6,4 +6,4 @@ ZABBIX_DIR=/etc/zabbix
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/zapgix
 cp -r ${SOURCE_DIR}/zapgix/sql ${ZABBIX_DIR}/scripts/agentd/zapgix/
 cp ${SOURCE_DIR}/zapgix/zapgix.sh ${ZABBIX_DIR}/scripts/agentd/zapgix/
-cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.d/zapgix.conf
+cp ${SOURCE_DIR}/zapgix/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agentd.conf.d/zapgix.conf
